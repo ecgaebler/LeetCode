70f0d7e8f37f3294cfca71568c46f6a8dd35f0bb
ecgaebler <evan.gaebler@gmail.com>
2020-04-03 00:17:05 -0700

    almost solved LC#532, but missed the case where k is negative.

---
+++ 0532.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findPairs(self, nums: List[int], k: int) -> int:
+        if k == 0:
+            doubles = set()
+            counted = set()
+            for num in nums:
+                if num in counted:
+                    doubles.add(num)
+                counted.add(num)
+            return len(doubles)
+        
+        counted = set()
+        pairs = set()
+        for num in nums:
+            if num not in counted:
+                counted.add(num)
+                if num - k in counted:
+                    pairs.add((num - k, num))
+                if num + k in counted:
+                    pairs.add((num, num + k))
+                    
+        return len(pairs)
