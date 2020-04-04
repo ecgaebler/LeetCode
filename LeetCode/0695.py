c0abf457f5cc695b07b84376aa5c4d9c70207458
ecgaebler <evan.gaebler@gmail.com>
2020-04-04 16:33:45 -0700

    continuing debugging with LC#695, but still stuck.

--- LeetCode/0695.py
+++ LeetCode/0695.py
@@ -1,4 +1,3 @@
-
 def maxAreaOfIsland(grid):
     height, width = len(grid), len(grid[0])
     
@@ -23,51 +22,49 @@ def maxAreaOfIsland(grid):
             if grid[y][x] == 1:
                 neighbors = find_neighbors(x, y, grid)
                 if neighbors:
-                    print("cell", (x,y), "has neighbors", neighbors)
+                    print("cell", (x,y), "has neighbors", neighbors) #DEBUG
                     #count unique neighbor ids
-                    neighbor_ids = []
+                    neighbor_ids = set()
                     for cell in neighbors:
                         if cell in cell_ids: #neighbor has a registered id
-#                            print("current cell: ", (x,y))
-#                            print("labeled neighbor found: ", cell_ids[cell], " at ", cell)
-                            neighbor_ids.append(cell_ids[cell])
+                            neighbor_ids.add(cell_ids[cell])
+                            print("neighbor_ids:", neighbor_ids) #DEBUG
                     if len(neighbor_ids) == 0: #no neighbors are registered
                         cell_ids[(x,y)] = next_id
                         cells_with_id[next_id] = set([(x,y)])
                         next_id += 1
                     elif len(neighbor_ids) == 1: #combine with only labeled neighbor island
-                        cell_ids[(x,y)] = neighbor_ids[0]
-                        cells_with_id[neighbor_ids[0]].add((x,y))
+                        cell_ids[(x,y)] = neighbor_ids.pop()
+                        cells_with_id[cell_ids[(x,y)]].add((x,y))
                     else: #mutiple neighbors with different ids
                         combined_id = min(neighbor_ids) #choose id to keep
-#                        print("multiple islands: ", neighbor_ids)
                         #find and relabel any neighbor islands
                         for neighbor_id in neighbor_ids:
                             if neighbor_id != combined_id:
                                 #relabel cells in neighbor island
+                                empty_ids = set()
                                 for cell in cells_with_id[neighbor_id]:
                                     cell_ids[cell] = combined_id #change cell's id
-                                    cells_with_id[neighbor_id].discard(cell) #remove from old set
                                     cells_with_id[combined_id].add(cell) #add to new set
-                                    if not cells_with_id[neighbor_id]:
-                                        del cells_with_id[neighbor_id] #remove unused id 
+                                del cells_with_id[neighbor_id] #remove now-unused id
                 else: #current cell is isolated
                     cell_ids[(x,y)] = next_id
                     cells_with_id[next_id] = set([(x,y)])
                     next_id += 1
 
-    for cell_id in cells_with_id:
-        print(cell_id, "for", list(cells_with_id[cell_id]))
-        
-#    print(cells_with_id)
+    for cell_id in cells_with_id:                           #DEBUG
+        print(cell_id, "for", list(cells_with_id[cell_id])) #DEBUG
+
+    if not cells_with_id:
+        return 0
     return max(len(cells_with_id[cell_id]) for cell_id in cells_with_id)
 
 #tests
 test1 = [[1,1,0,0,0],[1,1,1,0,0],[0,0,0,1,1],[0,0,0,1,1]]
 test2 = [[1,1,1,1,0],[1,1,1,1,0],[1,1,1,1,0],[0,0,0,0,1]]
-tests = [test1,test2]
+test3 = [[1,0,1],[1,1,1],[0,0,1]]
+tests = [test3]
 for test in tests:
     for row in test:
         print(row)
     print(maxAreaOfIsland(test))
-
