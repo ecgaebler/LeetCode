22a87b93143946af8dab630ebb3720e760352cc2
ecgaebler <evan.gaebler@gmail.com>
2020-04-04 17:18:48 -0700

    Made code cleaner and simpler. Strangely, this actually made the algorithm slightly slower (but still 'optimal'). I'm guessing because it might take longer to compare ints than booleans.

--- LeetCode/0058.py
+++ LeetCode/0058.py
@@ -1,14 +1,11 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        found_nonspace = False #in case of trailing spaces at end
         length = 0
         for i in range(len(s) - 1, -1, -1):
             if s[i] == " ":
-                if found_nonspace:
+                if length > 0:
                     return length
             else:
-                if not found_nonspace:
-                    found_nonspace = True
                 length += 1
         return length
         
