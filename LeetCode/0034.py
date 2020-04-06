d226273b073897df1b75a44d4074dab2ec443086
ecgaebler <evan.gaebler@gmail.com>
2020-04-05 19:15:26 -0700

    updated algorithm to make it start with one binary search that splits into two once it finds an instance of the target. Doesn't seem to have actually made it faster though, for some reason.

--- LeetCode/0034.py
+++ LeetCode/0034.py
@@ -1,43 +1,52 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
+        if not nums:
+            return [-1, -1]
         
-        def findFirst(nums, target):
-            start = 0
-            end = len(nums) - 1
-        
-            while start <= end:
-                mid = start + (end - start) // 2
-            
-                if nums[mid] == target:
-                    if (mid == 0 or target > nums[mid-1]):
-                        return mid
-                    else:
-                        end = mid - 1
-                elif target < nums[mid]:
-                    end = mid - 1
+        def first_between(array, target, l, r):
+            """ Find the first instance of a value between index l and r. """
+            l, r = 0, len(array) - 1
+            while l + 1 < r:
+                mid = l + (r - l) // 2
+                if array[mid] < target:
+                    l = mid
                 else:
-                    start = mid + 1
-                    
+                    r = mid
+            #post-processing
+            if array[l] == target:
+                return l
+            elif array[r] == target:
+                return r
             return -1
         
-        def findLast(nums, target):
-            start = 0
-            end = len(nums) - 1
-            
-            while start <= end:
+        def last_between(array, target, l, r):
+            """ Find the last instance of a value between index l and r. """
+            l, r = 0, len(array) - 1
+            while l + 1 < r:
+                mid = l + (r - l) // 2
+                if array[mid] <= target:
+                    l = mid
+                else:
+                    r = mid
+            #post-processing
+            if array[r] == target:
+                return r
+            elif array[l] == target:
+                return l
+            return -1
+        
+        #binary search until you find the target, then split from there
+        start, end = 0, len(nums) - 1
+        while start + 1 < end:
                 mid = start + (end - start) // 2
+                if nums[mid] < target:
+                    start = mid
+                elif nums[mid] > target:
+                    end = mid
+                else: #if nums[mid] == target, split binary search from here to find both edges.
+                    break
+        first_target = first_between(nums, target, start, end)
+        last_target = last_between(nums, target, start, end)
+        return [first_target, last_target]
                 
-                if nums[mid] == target:
-                    if (mid == len(nums)-1 or target < nums[mid+1]):
-                        return mid
-                    else:
-                        start = mid + 1
-                elif target < nums[mid]:
-                    end = mid - 1
-                else:
-                    start = mid + 1
-            
-            return -1
-        
-        return [findFirst(nums, target), findLast(nums, target)]
-    
+        return [-1,-1]
