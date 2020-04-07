13f00c73296c869528ed4393ce7ba07a5e0d21d7
ecgaebler <evan.gaebler@gmail.com>
2020-04-06 22:22:12 -0700

    Simplified code.

--- LeetCode/0539.py
+++ LeetCode/0539.py
@@ -6,18 +6,15 @@ class Solution:
             hours2, minutes2 = time2.split(":")
             time1 = int(hours1) * 60 + int(minutes1)
             time2 = int(hours2) * 60 + int(minutes2)
-            forward_difference = abs(time2 - time1)
-            time1 += 24 * 60 #shift earlier time forward 24 hours
-            wraparound_difference = abs(time2 - time1)
-            return min(forward_difference, wraparound_difference)
+            return time2 - time1
             
         min_difference = float("inf")
         timeline = sorted(timePoints)
         
-        for i in range(0, len(timeline) - 1):
-            min_difference = min(min_difference, difference(timeline[i], timeline[i+1]))
+        for i in range(1, len(timeline)):
+            min_difference = min(min_difference, difference(timeline[i-1], timeline[i]))
         #also check time between beginning and end of timeline, due to wraparound.
-        min_difference = min(min_difference, difference(timeline[0], timeline[-1]))
+        min_difference = min(min_difference, difference(timeline[-1], timeline[0]) + 1440)
         
         return min_difference
         
