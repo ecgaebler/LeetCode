0e4d2e6496f5f5e90b1dc714231d29896525bb90
ecgaebler <evan.gaebler@gmail.com>
2020-04-07 00:22:22 -0700

    submitted LC#443, but bugs still remain. I'll have to come back to this later.

---
+++ LeetCode/0443.py
@@ -0,0 +1,36 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        if not chars:
+            return 0
+        char_count = 0
+        write_idx = 0
+        current_char = chars[0]
+        
+        for scan_idx in range(0, len(chars)):
+            if chars[scan_idx] == current_char:
+                char_count += 1
+            else:
+                if char_count > 1:
+                    chars[write_idx] = current_char
+                    write_idx += 1
+                    temp = str(char_count)
+                    for char in temp:
+                        chars[write_idx] = char        
+                        write_idx += 1
+                else:
+                    write_idx += 1
+                char_count = 1
+                current_char = chars[scan_idx]
+                
+        if char_count > 1:
+            chars[write_idx] = current_char
+            write_idx += 1
+            temp = str(char_count)
+            for char in temp:
+                chars[write_idx] = char       
+                write_idx += 1
+        else:
+            chars[write_idx] = current_char
+            write_idx += 1
+        del chars[write_idx:]
+        return len(chars)
