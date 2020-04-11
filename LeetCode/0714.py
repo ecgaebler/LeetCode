61aaa07cbd0ead11c6751531c0255ae755adca26
ecgaebler <evan.gaebler@gmail.com>
2020-04-11 14:49:18 -0700

    properly solved LC#714. Time O(N), space O(1). The test cases online are faulty: they should include a fee for both selling and buying, but only include it for one, not both.

--- LeetCode/0714.py
+++ LeetCode/0714.py
@@ -1,33 +1,13 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        day, have_stock, profit,  = 0, False, 0
-        last_day = len(prices) - 1
-        max_profit = 0
-        calculated = {} #transaction format is {(day, have_stock): max_gain} 
-        stack = [(day, have_stock, profit)]
-        while stack:
-            day, have_stock, profit = stack[-1]
-            if (day, have_stock) in calculated: #check if already calculated
-                profit = profit + calculated[(day, have_stock)]
-                max_profit = max(max_profit, profit)
-                stack.pop() #remove from stack, now that final gain was calculated
+        with_stock = 0
+        without_stock = 0
+        for day, price in enumerate(prices):
+            if day == 0:
+                with_stock = - price - fee
+                without_stock = 0
             else:
-                if day == last_day:
-                    if have_stock: 
-                        profit = profit + prices[day] #sell stock
-                    calculated[(day, have_stock)] = profit #record max gain
-                else:
-                    #sell stock
-                    if have_stock:
-                        sell_stock = (day + 1, False, profit + prices[day])
-                        stack.append(sell_stock)
-                    #don't sell or buy
-                    no_action = (day + 1, have_stock, profit)
-                    stack.append(no_action)
-                    #buy stock if possible
-                    if not have_stock:
-                        buy_stock = (day + 1, True, profit - (prices[day] + fee))
-                        stack.append(buy_stock)
-        return max_profit
-                    
-                    
+                with_stock = max(with_stock, without_stock - price - fee)
+                without_stock = max(without_stock, with_stock + price)
+                #there should be a fee for both selling and buying, but the test cases are faulty.
+        return without_stock
