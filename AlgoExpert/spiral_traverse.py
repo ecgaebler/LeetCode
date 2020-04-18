1a65c909f5efec19490aeca7cbb327cdfd7de24c
ecgaebler <evan.gaebler@gmail.com>
2020-04-18 14:54:54 -0700

    submitted much cleaner, more efficient code. Time and space are O(N).

--- AlgoExpert/spiral_traverse.py
+++ AlgoExpert/spiral_traverse.py
@@ -1,40 +1,30 @@
 def spiralTraverse(matrix):
-	if len(matrix) == 0 or len(matrix[0]) == 0:
-		return matrix
-	
-    def nextCell(matrix, visited, x, y, direction):
-		turns = 0
-		if direction == "E":
-			if x < len(matrix[0]) - 1 and (x+1, y) not in visited:
-				return [x+1, y, direction]
-			direction = "S"
-			turns += 1
-		if direction == "S":
-			if y < len(matrix) - 1 and (x, y+1) not in visited:
-				return [x, y+1, direction]
-			direction = "W"
-			turns += 1
-		if direction == "W":
-			if x > 0 and (x-1, y) not in visited == 0:
-				return [x-1, y, direction]
-			direction = "N"
-			turns += 1
-		if direction == "N":
-			if y > 0 and (x, y-1) not in visited == 0:
-				return [x, y+1, direction]
-			direction = "E"
-			turns += 1
-		if turns > 1:
-			return [-1, -1, "X"]
-		return [x, y, direction]
-	
-	x, y, direction = 0, 0, "E"
-	visited = set()
 	result = []
+    if not matrix or not matrix[0]:
+		return result
+	startX, endX, startY, endY = 0, len(matrix[0]) - 1, 0, len(matrix) - 1
 	
-	while direction != "X":
-		result.append(matrix[y][x])
-		visited.add((x, y))
-		x, y, direction = nextCell(matrix, visited, x, y, direction)
-		
+	while startX <= endX and startY <= endY:
+		for x in range(startX, endX + 1):
+			result.append(matrix[startY][x])
+			
+		for y in range(startY + 1, endY + 1):
+			result.append(matrix[y][endX])
+			
+		for x in reversed(range(startX, endX)):
+			if startY == endY:
+				break
+			result.append(matrix[endY][x])
+			
+		for y in reversed(range(startY + 1, endY)):
+			if startX == endX:
+				break
+			result.append(matrix[y][startX])
+				
+		startY += 1
+		endX -= 1
+		startX += 1
+		endY -= 1
+			
 	return result
+		
