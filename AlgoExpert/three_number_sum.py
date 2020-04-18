a6d92ecd0007a8e4af73509cedcb269eb00d9ae8
ecgaebler <evan.gaebler@gmail.com>
2020-04-17 22:19:53 -0700

    Optimal solution.

--- AlgoExpert/three_number_sum.py
+++ AlgoExpert/three_number_sum.py
@@ -1,31 +1,17 @@
 def threeNumberSum(array, targetSum):
-	val_indexes = {}
-	for idx, num in enumerate(array):
-		if num not in val_indexes:
-			val_indexes[num] = []
-		val_indexes[num].append(idx)
-		
-	def two_sum_ignore(target, ignore_idx):
-		pairs = set()
-		for idx1, num1 in enumerate(array):
-			if idx1 == ignore_idx:
-				continue
-			if target-num1 in val_indexes:
-				for idx in val_indexes[target-num1]:
-					if idx != idx1 and idx != ignore_idx:
-						pairs.add(tuple(sorted([num1, target-num1])))
-						break
-		return pairs
-		
-	triples = set()
-    for i0, n0 in enumerate(array):
-		for pair in two_sum_ignore(targetSum - n0, i0):
-			unpacked = list(pair).append(n0)
-			triples.add(tuple(sorted(unpacked)))
-	
-	triple_list=sorted(list(triples), key=lambda x: x[0])
-	result = []
-	for triple in triple_list:
-		result.append(triple)
-	return result
-		
+	sorted_array = sorted(array)
+	triplets = []
+	for i in range(len(array) - 2):
+		left = i + 1
+		right = len(array) - 1
+		while left < right:
+			current_sum = sorted_array[i] + sorted_array[left] + sorted_array[right]
+			if current_sum < targetSum:
+				left += 1
+			elif current_sum > targetSum:
+				right -= 1
+			else:
+				triplets.append([sorted_array[i], sorted_array[left], sorted_array[right]])
+				left += 1
+				right -= 1
+	return triplets
