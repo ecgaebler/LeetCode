8b11a074046ca662823bcc9d1ed286ea23b5aa8f
ecgaebler <evan.gaebler@gmail.com>
2020-04-20 14:55:48 -0700

    updated solution to take O(1) space

--- AlgoExpert/max_subset_sum_no_adjacent.py
+++ AlgoExpert/max_subset_sum_no_adjacent.py
@@ -1,11 +1,13 @@
-#input: 7, 10, 12, 7, 9, 14
 def maxSubsetSumNoAdjacent(array):
 	if len(array) == 0:
 		return 0
 	elif len(array) < 3:
 		return max(array)
 	
-    max_after = {0:array[0], 1:max(array[1], array[0])}
-	for idx in range(2, len(array)):
-		max_after[idx] = max(max_after[idx - 1], array[idx] + max_after[idx - 2])
-	return max_after[(len(array) - 1)]
+    back2, back1 = array[0], max(array[1], array[0])
+	for idx in range(2, len(array)):		
+		current_max = max(back1, array[idx] + back2)
+		back2 = back1
+		back1 = current_max
+
+	return current_max
