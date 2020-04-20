99b829501552d61991128e8ae0eb330ab6d9c8c3
ecgaebler <evan.gaebler@gmail.com>
2020-04-19 17:46:49 -0700

    The reason my previous solution didn't work was a mistake in the question prompt. I've fixed it to pass the test cases. I've also added a commented, recursive solution.

--- AlgoExpert/validate_bst.py
+++ AlgoExpert/validate_bst.py
@@ -18,7 +18,7 @@ def validateBst(tree):
 		current, min_value, max_value = queue.popleft()
 		if current is None:
 			continue
-		if current.value > max_value or current.value <= min_value:
+		if current.value >= max_value or current.value < min_value:
 			return False
 		#if current.left is not None and current.left.value >= current.value:
 		#	return False
@@ -27,3 +27,30 @@ def validateBst(tree):
 		queue.append((current.left, min_value, current.value))
 		queue.append((current.right, current.value, max_value))
 	return True
+
+'''
+#here's a recursive solution:
+from collections import deque
+
+def validateBst(tree):
+	if tree is None:
+		return True
+	min_value = float("-inf")
+	max_value = float("inf")
+	
+	def validate_helper(node, min_val, max_val):
+		if node is None:
+			return True
+		if node.value < min_val or node.value >= max_val:
+			return False
+		if node.left is not None:
+			if not validate_helper(node.left, min_val, node.value):
+				return False
+		if node.right is not None:
+			if not validate_helper(node.right, node.value, max_val):
+				return False
+		return True
+	
+	return validate_helper(tree, min_value, max_value)
+
+'''
