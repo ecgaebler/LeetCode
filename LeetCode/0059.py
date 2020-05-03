e962e80393cb2e4fb1486ab633da4844fe8681a4
ecgaebler <evan.gaebler@gmail.com>
2020-05-02 18:24:48 -0700

    code is now a lot cleaner, using for loops in different directions with moving bounds, rather than explicitely keeping track of direction of traversal.

--- LeetCode/0059.py
+++ LeetCode/0059.py
@@ -1,51 +1,25 @@
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
-        
-        def update(matrix, x, y, direction):
-            if direction == "E":
-                #if you can advance in current direction...
-                if x < len(matrix) - 1 and matrix[y][x + 1] == None:
-                    #move into new square
-                    x += 1
-                    return (x, y, direction)
-                #if you can't advance, try turning.
-                direction = "S"
-                    
-            if direction == "S":
-                if y < len(matrix) - 1 and matrix[y + 1][x] == None:
-                    y += 1
-                    return (x, y, direction)
-                direction = "W"
-                
-            if direction == "W":
-                if x > 0 and matrix[y][x - 1] == None:
-                    x -= 1
-                    return (x, y, direction)
-                direction = "N"
-                
-            if direction == "N":
-                if y > 0 and matrix[y - 1][x] == None:
-                    y -= 1
-                    return (x, y, direction)
-                direction = "E"
-            
-            #do a final check to see if next square is open after turning from N to E
-            if x < len(matrix) - 1 and matrix[y][x + 1] == None:
-                x += 1
-                return (x, y, direction)
-            else:
-                return (x, y, None)
-                    
-                    
-        matrix = [[None]*n for _ in range(n)] #initialize empty array to be filled
-        x, y, direction = 0, 0, "E"
-            
-        for i in range(1, n**2 + 1):
-            matrix[y][x] = i
-            x, y, direction = update(matrix, x, y, direction)
-            if not direction:
-                break
-            
-        return matrix
-            
+        result = [ [[] for _ in range(n)] for _ in range(n) ]
+        y_start, y_end = 0, n 
+        x_start, x_end = 0, n
+        counter = 1
+        while y_start < y_end and x_start < x_end:
+            for x in range(x_start, x_end): #top side
+                result[y_start][x] = counter
+                counter += 1
+            for y in range(y_start + 1, y_end): #right side
+                result[y][x_end - 1] = counter
+                counter += 1
+            for x in range(x_end - 2, x_start - 1, -1): #bottom side
+                result[y_end - 1][x] = counter
+                counter += 1
+            for y in range(y_end - 2, y_start, -1): #left side
+                result[y][x_start] = counter
+                counter += 1
+            y_start += 1
+            y_end -= 1
+            x_start += 1
+            x_end -= 1
+        return result
                 
