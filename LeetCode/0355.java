efe61a0a6d7fad384541b10b4f45034979f85e0f
ecgaebler <evan.gaebler@gmail.com>
2020-05-19 10:28:42 -0700

    Added some comments to 0355.java

--- LeetCode/0355.java
+++ LeetCode/0355.java
@@ -16,20 +16,20 @@ class Twitter {
     
     /** Compose a new tweet. */
     public void postTweet(int userId, int tweetId) {
-        tweets.addFirst(new Pair<Integer, Integer>(tweetId, userId));
+        tweets.addFirst(new Pair<Integer, Integer>(tweetId, userId)); // Put most recent tweet at front
         tweetOwner.put(tweetId, userId);
-        follow(userId, userId);
+        follow(userId, userId); // Make sure user is following self
     }
     
     /** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. */
     public List<Integer> getNewsFeed(int userId) {
-        if(!followedBy.containsKey(userId)) {
+        if(!followedBy.containsKey(userId)) { // User isn't following anyone
             return Collections.emptyList();
         }
         List<Integer> displayTweets = new LinkedList<>();
-        for(Pair<Integer, Integer>tweet: tweets) {
+        for(Pair<Integer, Integer>tweet: tweets) { // Loop through all tweets to find ones user is following
             if(followedBy.get(userId).contains(tweet.getValue())) {
-                displayTweets.add(tweet.getKey());
+                displayTweets.add(tweet.getKey()); 
             }
             if(displayTweets.size() == 10) {
                 break;
@@ -40,13 +40,13 @@ class Twitter {
     
     /** Follower follows a followee. If the operation is invalid, it should be a no-op. */
     public void follow(int followerId, int followeeId) {
-        if(!followedBy.containsKey(followeeId)) {
-            followedBy.put(followeeId, new HashSet<Integer>());
-            followersOf.put(followeeId, new HashSet<Integer>());
-            follow(followeeId, followeeId);
+        if(!followedBy.containsKey(followeeId)) { // Followee doesn't have any followers, including self
+            followedBy.put(followeeId, new HashSet<Integer>()); // Make new hashset of followers for followee
+            followersOf.put(followeeId, new HashSet<Integer>()); // Make set of users that followee follows (so they can follow self)
+            follow(followeeId, followeeId); // Make followee follow self
         }
-        if(!followedBy.containsKey(followerId)) {
-            followedBy.put(followerId, new HashSet<Integer>());
+        if(!followedBy.containsKey(followerId)) { // Follower doesn'f follow anyone, including self
+            followedBy.put(followerId, new HashSet<Integer>()); // Make new hashset of users following follower (so they can follow self)
             followersOf.put(followerId, new HashSet<Integer>());
             follow(followerId, followerId);
         }
