52902fb9ab4c0ce090e41afdf82faa9c46bc218c
ecgaebler <evan.gaebler@gmail.com>
2021-02-02 12:09:57 -0800

    one line solution with time O(N).

--- LeetCode/0520.py
+++ LeetCode/0520.py
@@ -1,18 +1,4 @@
 def detectCapitalUse(word):
-    all_lower = True
-    all_upper = True
-    init_cap = True
-    if word[0].islower():
-        init_cap = all_upper = False
-    else:
-        all_lower = False
-        
-    for i in range(1, len(word)):
-        if word[i].isupper():
-            init_cap = all_lower = False
-        else:
-            all_upper = False
-        if not (all_lower or all_upper or init_cap):
-            return False
-        
-    return all_lower or all_upper or init_cap
+        return (word.isupper() or
+                word.islower() or
+                (word[0].isupper() and word[1:].islower()))
