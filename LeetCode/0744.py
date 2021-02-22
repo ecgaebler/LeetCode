d7d656783b9dffcb3b14d1f305724885b751c58c
ecgaebler <evan.gaebler@gmail.com>
2021-02-22 12:05:25 -0800

    My solution now works. Time O(log(N)), space O(1).

--- LeetCode/0744.py
+++ LeetCode/0744.py
@@ -3,16 +3,34 @@ def nextGreatestLetter(letters, target):
     
     while l + 1 < r:
         mid = l + (r - l) // 2
-        if ord(letters[mid]) < ord(target):
+        if ord(letters[mid]) <= ord(target):
             l = mid
         else:
             r = mid
     
+    if ord(letters[r]) <= ord(target):
+        return letters[0]
     if ord(letters[l]) > ord(target):
         return letters[0]
-    elif ord(letters[l]) == ord(target):
-        return letters[(l + 1) % len(letters)]
-    elif ord(letters[r]) > ord(target):
+    if ord(letters[l]) <= ord(target):
         return letters[r]
-    else:
-        return letters[0]
+    return letters[l]
+
+#TEST CODE
+tests = [
+    (["c","f","j"],"a","c"),
+    (["c","f","j"],"c","f"),
+    (["c","f","j"],"d","f"),
+    (["c","f","j"],"g","j"),
+    (["c","f","j"],"j","c"),
+    (["c","f","j"],"k","c"),
+    (["e","e","e","e","e","e","n","n","n","n"],"e","n"),
+    (["e","e","e","e","e","e","n","n","n","n"],"n","e")]
+
+do_tests = True
+if do_tests:
+    for letters, target, answer in tests:
+        if nextGreatestLetter(letters, target) == answer:
+            print("Test PASS") 
+        else:
+            print("Test FAIL")
